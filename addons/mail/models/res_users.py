239b1b885f1057563f48fa4e3aeedd67f5a3010c
samuelsalvadorjr16 <samuel.salvador.jr16@gmail.com>
2020-12-07 00:22:40 +0000

    Added Changes Remove the About in General Setting *Remove the Refferal (Gift Icon) *Change Odoobot to PMSimply

--- addons/mail/models/res_users.py
+++ addons/mail/models/res_users.py
@@ -20,11 +20,11 @@ class Users(models.Model):
 
     notification_type = fields.Selection([
         ('email', 'Handle by Emails'),
-        ('inbox', 'Handle in Odoo')],
+        ('inbox', 'Handle in PMSimply')],
         'Notification', required=True, default='email',
         help="Policy on how to handle Chatter notifications:\n"
              "- Handle by Emails: notifications are sent to your email address\n"
-             "- Handle in Odoo: notifications appear in your Odoo Inbox")
+             "- Handle in PMSimply: notifications appear in your Odoo Inbox")
     # channel-specific: moderation
     is_moderator = fields.Boolean(string='Is moderator', compute='_compute_is_moderator')
     moderation_counter = fields.Integer(string='Moderation count', compute='_compute_moderation_counter')
